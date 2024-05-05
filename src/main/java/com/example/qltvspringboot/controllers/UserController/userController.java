48ae0c1147aba1287441b01d2e9ebff2c4f7b89e
Minh Khang <94234570+khanglag@users.noreply.github.com>
2024-05-06 02:14:52 +0700

    Update userController.java

--- src/main/java/com/example/qltvspringboot/controllers/UserController/userController.java
+++ src/main/java/com/example/qltvspringboot/controllers/UserController/userController.java
@@ -1,24 +1,54 @@
+// package com.example.qltvspringboot.controllers.UserController;
+
+// import ch.qos.logback.core.model.Model;
+// import com.example.qltvspringboot.entity.ThanhVien;
+// import com.example.qltvspringboot.service.ThanhVienService;
+
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.stereotype.Controller;
+// import org.springframework.web.bind.annotation.ModelAttribute;
+// import org.springframework.web.bind.annotation.PostMapping;
+// import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.RequestParam;
+
+// @Controller
+// public class userController {
+
+//     @Autowired
+//     private ThanhVienService thanhVienService;
+
+//     @Autowired
+//     private ThanhVien _ThanhVienBean;
+
+//     @RequestMapping("/signin")
+//     public String Signin() {
+//         return "View/signin";
+//     }
+
+//     @RequestMapping("/register")
+//     public String Register() {
+//         return "View/register";
+//     }
+
+//     @PostMapping("/checklogin")
+//     public String Login(@RequestParam("MSSV") int MSSV, @RequestParam("Password") String Password,
+//             Model model) {
+//         if (thanhVienService.checkMSSV(_ThanhVienBean.getMaTV()) == false) {
+//             return "redirect:/signin?mssvwrong";
+//         }
+//         if (thanhVienService.checkPasswordThanhVien(_ThanhVienBean.getMaTV(), _ThanhVienBean.getPassword())) {
+//             return "redierct:/home?success";
+//         }
+//         return "redirect:/signin?passwordwrong";
+//     }
+// }
 package com.example.qltvspringboot.controllers.UserController;
 
-import ch.qos.logback.core.model.Model;
-import com.example.qltvspringboot.entity.ThanhVien;
-import com.example.qltvspringboot.service.ThanhVienService;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class userController {
-
-    @Autowired
-    private ThanhVienService thanhVienService;
-
-    @Autowired
-    private ThanhVien _ThanhVienBean;
 
     @RequestMapping("/signin")
     public String Signin() {
@@ -28,17 +58,5 @@ public class userController {
     @RequestMapping("/register")
     public String Register() {
         return "View/register";
-    }
-
-    @PostMapping("/checklogin")
-    public String Login(@RequestParam("MSSV") int MSSV, @RequestParam("Password") String Password,
-            Model model) {
-        if (thanhVienService.checkMSSV(_ThanhVienBean.getMaTV()) == false) {
-            return "redirect:/signin?mssvwrong";
-        }
-        if (thanhVienService.checkPasswordThanhVien(_ThanhVienBean.getMaTV(), _ThanhVienBean.getPassword())) {
-            return "redierct:/home?success";
-        }
-        return "redirect:/signin?passwordwrong";
     }
 }
