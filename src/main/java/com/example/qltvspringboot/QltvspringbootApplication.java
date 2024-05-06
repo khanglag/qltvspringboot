f668041437ff1449d7e8e6d87255ca3ac94cfaa9
Minh Khang <94234570+khanglag@users.noreply.github.com>
2024-05-06 11:15:15 +0700

    r

--- src/main/java/com/example/qltvspringboot/QltvspringbootApplication.java
+++ src/main/java/com/example/qltvspringboot/QltvspringbootApplication.java
@@ -2,12 +2,23 @@ package com.example.qltvspringboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.example.qltvspringboot.entity.ThanhVien;
 
 @SpringBootApplication
 public class QltvspringbootApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(QltvspringbootApplication.class, args);
+	}
+
+	@Bean(name = "THANHVIEN_BEAN")
+	public ThanhVien setthanhvien() {
+		ThanhVien thanhvien = new ThanhVien();
+		thanhvien.setMaTV(123456);
+		thanhvien.setPassword("pass");
+		return thanhvien;
 	}
 
 }
--- src/main/java/com/example/qltvspringboot/controllers/UserController/userController.java
+++ src/main/java/com/example/qltvspringboot/controllers/UserController/userController.java
@@ -1,10 +1,23 @@
 package com.example.qltvspringboot.controllers.UserController;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.qltvspringboot.entity.ThanhVien;
+import com.example.qltvspringboot.service.ThanhVienService;
 
 @Controller
 public class userController {
+    @Autowired
+    ThanhVienService thanhVienService;
+
+    @Autowired
+    ThanhVien _thanhvienbean;
 
     @RequestMapping("/signin")
     public String Signin() {
@@ -14,5 +27,20 @@ public class userController {
     @RequestMapping("/register")
     public String Register() {
         return "View/register";
+    }
+
+    @PostMapping("/checkLogin")
+    public String checkLogin(@RequestParam("MSSV") int MSSV, @RequestParam("Password") String password) {
+
+        if ((_thanhvienbean.getMaTV() == MSSV) && _thanhvienbean.getPassword().equals(password)) {
+            System.out.println("Login successful");
+            return "View/user";
+        }
+        return "View/sign";
+    }
+
+    @GetMapping("/signin")
+    public String logout() {
+        return "View/sign";
     }
 }
