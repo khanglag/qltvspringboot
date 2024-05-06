0daa44717aa240bd91981f72c953a05828d7d07a
Minh Khang <94234570+khanglag@users.noreply.github.com>
2024-05-06 21:38:08 +0700

    t

--- src/main/java/com/example/qltvspringboot/QltvspringbootApplication.java
+++ src/main/java/com/example/qltvspringboot/QltvspringbootApplication.java
@@ -2,12 +2,25 @@ package com.example.qltvspringboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+
+import com.example.qltvspringboot.entity.ThanhVien;
 
 @SpringBootApplication
+@ComponentScan("com.example.qltvspringboot.repository")
 public class QltvspringbootApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(QltvspringbootApplication.class, args);
+	}
+
+	@Bean(name = "ThanhVienBean")
+	public ThanhVien setThanhVien() {
+		ThanhVien thanhVien = new ThanhVien();
+		thanhVien.setMaTV(123456);
+		thanhVien.setPassword("pass");
+		return thanhVien;
 	}
 
 }
